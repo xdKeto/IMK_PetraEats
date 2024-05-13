a664f6778085405a2dd63a86935bc4c7c194176b
xdKeto <128332966+xdKeto@users.noreply.github.com>
2024-05-13 11:04:40 +0700

    added sweetalerts to login page

--- resource/js/mahasiswa_login.js
+++ resource/js/mahasiswa_login.js
@@ -2,9 +2,32 @@ function validateLogin() {
   var username = document.getElementById("email").value;
   var password = document.getElementById("password").value;
 
-  if (username === "c14220331@gmail.com" && password === "c14220331") {
-    alert("Login successful!");
-  } else {
-    alert("Invalid username or password. Please try again.");
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    },
+  });
+
+  if (username === "c14220331@john.petra.ac.id" && password === "c14220331") {
+    Toast.fire({
+      icon: "success",
+      title: "Log in succesful",
+    });
+  } else if(username === "" && password === ""){
+    Toast.fire({
+        icon: "info",
+        title: "Enter your username and password",
+      });
+  }else {
+    Toast.fire({
+      icon: "error",
+      title: "Please enter your username and password correctly",
+    });
   }
 }
