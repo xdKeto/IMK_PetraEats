b4c358ab91418f8b9bdef65f43271368f05f328b
Daniel <39670536+danielkg123@users.noreply.github.com>
2024-05-17 11:57:57 +0700

    js for dropdown logout mahassiwa
    linked to landing page etc in the future

---
+++ resource/js/navbar.js
@@ -0,0 +1,13 @@
+function logoutDropdown() {
+    document.getElementById("name-dropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("name-dropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
