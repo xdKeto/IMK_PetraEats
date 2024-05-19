1f8707831273b085ab4369e9c8c23cf3d96a7447
FlyingLemonade <christopher.ryan7231@gmail.com>
2024-05-19 22:52:22 +0700

    bnerin navbar

--- resource/js/navbar.js
+++ resource/js/navbar.js
@@ -12,3 +12,48 @@ window.onclick = function (e) {
     }
   }
 };
+
+// Keranjang
+const cartAlert = document.getElementById("cart-alert");
+const keranjangBtn = document.getElementById("navitem-keranjang");
+
+keranjangBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const counters = document.querySelectorAll(".food-counter");
+  let allZero = true;
+
+  counters.forEach((counter) => {
+    if (parseInt(counter.textContent) > 0) {
+      allZero = false;
+    }
+  });
+
+  if (cartAlert.style.display === "none" || allZero) {
+    Swal.fire({
+      title: "Keranjang kosong !",
+      text: "Yuk pesan dulu :)",
+      icon: "error",
+      confirmButtonColor: "#d33",
+      confirmButtonText: "Okay",
+    });
+  } else {
+    Swal.fire({
+      title: "Cek keranjang ?",
+      text: "Sudah selesai mesannya ?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#0095e7",
+      cancelButtonColor: "#bbbbbb",
+      confirmButtonText: "Sudah",
+      cancelButtonText: "Tambah lagi deh",
+      reverseButtons: true, // This option moves the confirm button to the right
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setTimeout(function () {
+          window.location = "../mahasiswa/mahasiswa_landing_page.html";
+        }, 300);
+      }
+    });
+  }
+});
