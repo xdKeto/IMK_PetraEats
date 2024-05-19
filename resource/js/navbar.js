5b1e084d075855c68651594b1bc7fa9b99c589ec
FlyingLemonade <christopher.ryan7231@gmail.com>
2024-05-19 21:19:25 +0700

    Fix Some

--- resource/js/navbar.js
+++ resource/js/navbar.js
@@ -1,13 +1,14 @@
 function logoutDropdown() {
-    document.getElementById("name-dropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
+  document.getElementById("name-dropdown").classList.toggle("show");
+  console.log("asdasd");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  if (!e.target.matches(".dropbtn")) {
     var myDropdown = document.getElementById("name-dropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
+    if (myDropdown.classList.contains("show")) {
+      myDropdown.classList.remove("show");
     }
   }
+};
